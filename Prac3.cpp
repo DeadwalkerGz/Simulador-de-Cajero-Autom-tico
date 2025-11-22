d842cfeb4cebc72542f5141d4b7bf2f0ffd5bcbc
DeadWalkerGz <jesusernestolopezbeltran@gmail.com>
2025-11-22 13:49:08 -0700

    2

--- Prac3.cpp
+++ Prac3.cpp
@@ -1,5 +1,5 @@
-struct Cuenta {
-    int id;              // número de cuenta
-    std::string titular; // nombre del dueño
-    double saldo;        // saldo actual
-};
+#include <iostream>   // Para imprimir y leer datos
+#include <string>     // Para usar std::string
+#include <stdexcept> // Para manejar excepciones
+
+using namespace std;  // Para no escribir std:: todo el tiempo
