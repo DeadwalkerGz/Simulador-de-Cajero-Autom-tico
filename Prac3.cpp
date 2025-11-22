9dab05f51d5300c83dd2770478ec61964b80628e
DeadWalkerGz <jesusernestolopezbeltran@gmail.com>
2025-11-22 13:55:59 -0700

    3

--- Prac3.cpp
+++ Prac3.cpp
@@ -1,5 +1,19 @@
-#include <iostream>   // Para imprimir y leer datos
-#include <string>     // Para usar std::string
-#include <stdexcept> // Para manejar excepciones
+#include <iostream>
+#include <string>
+#include <stdexcept>
+using namespace std;
 
-using namespace std;  // Para no escribir std:: todo el tiempo
+struct Cuenta { // Estructura para representar una cuenta bancaria
+    int id;          // Numero de cuenta
+    string titular;  // Nombre del dueño
+    double saldo;    // Saldo actual
+};
+
+
+const int NUM_CUENTAS = 3; // Numero de cuentas predefinidas
+
+Cuenta cuentas[NUM_CUENTAS] = { // Arreglo de cuentas predefinidas
+    {1001, "Juan Perez", 1500.0}, // Cuenta predefinida
+    {1002, "Maria Lopez", 2500.0}, // Cuenta predefinida
+    {1003, "Carlos Ruiz", 500.0} // Cuenta predefinida
+};
